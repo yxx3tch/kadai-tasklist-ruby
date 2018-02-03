6a8e00e305f100dc3ce127489fa1e245efefcc1a
EC2 Default User <ec2-user@ip-172-31-35-155.us-east-2.compute.internal>
2018-02-03 08:36:17 +0000

    bootstrap design

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,7 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   def index
-    @tasks = Task.all
+    @tasks = Task.all.page(params[:page])
   end
 
   def show
